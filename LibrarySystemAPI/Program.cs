9bbe2caed98ec1bbb17d978096a81cde3a89d716
ross-g-revpro <rossw3653@gmail.com>
2024-06-05 15:33:31 -0400

    more config updates

--- LibrarySystemAPI/Program.cs
+++ LibrarySystemAPI/Program.cs
@@ -2,10 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PracticeContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
