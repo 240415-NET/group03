d60ad766c3e1579480324d2351c625b8413af8d6
florespino <michael47265@yahoo.com>
2024-06-07 15:21:48 -0400

    setup the context, migrated and updated

---
+++ LibrarySystemAPI/Migrations/20240607190747_InitialCreate.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibrarySystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    barcode = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    genre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.barcode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    userId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    userName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.userId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Checkouts",
+                columns: table => new
+                {
+                    checkoutId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    dueDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    checkoutUseruserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    checkoutBookbarcode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checkouts", x => x.checkoutId);
+                    table.ForeignKey(
+                        name: "FK_Checkouts_Books_checkoutBookbarcode",
+                        column: x => x.checkoutBookbarcode,
+                        principalTable: "Books",
+                        principalColumn: "barcode",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Checkouts_Users_checkoutUseruserId",
+                        column: x => x.checkoutUseruserId,
+                        principalTable: "Users",
+                        principalColumn: "userId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checkouts_checkoutBookbarcode",
+                table: "Checkouts",
+                column: "checkoutBookbarcode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checkouts_checkoutUseruserId",
+                table: "Checkouts",
+                column: "checkoutUseruserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Checkouts");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
