ca7c9da7f68f7c206b672443b657aee390e8215d
florespino <michael47265@yahoo.com>
2024-06-07 15:21:48 -0400

    setup the context, migrated and updated

--- LibrarySystemAPI/04_DataAccess/LibrarySystemContext.cs
+++ LibrarySystemAPI/04_DataAccess/LibrarySystemContext.cs
@@ -5,5 +5,27 @@ namespace LibrarySystem.API.Data;
 
 public class LibrarySystemContext : DbContext
 {
-    
+
+    public DbSet<User> Users {get; set;}
+    public DbSet<Book> Books {get; set;}
+
+    public DbSet<Checkout> Checkouts {get; set;}
+
+    public LibrarySystemContext () {}
+
+    public LibrarySystemContext(DbContextOptions options) : base (options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {   
+        modelBuilder.Entity<Book>()
+            .ToTable("Books");
+        modelBuilder.Entity<Checkout>()
+            .ToTable("Checkouts");
+
+        modelBuilder.Entity<User>()
+            .ToTable("Users");
+
+        modelBuilder.UseCollation("SQL_Latin1_General_CP1_CS_AS");
+
+    }   
 }
