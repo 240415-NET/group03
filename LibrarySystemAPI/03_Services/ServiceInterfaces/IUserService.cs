403aa5bf9a153cb010f0ea2802a1680a861a971b
yennyFlores <yennyd.data@gmail.com>
2024-06-10 13:06:33 -0400

    adding a user to database

--- LibrarySystemAPI/03_Services/ServiceInterfaces/IUserService.cs
+++ LibrarySystemAPI/03_Services/ServiceInterfaces/IUserService.cs
@@ -1,6 +1,10 @@
+
+using LibrarySystem.API.Models;
+using LibrarySystem.API.Data;
+
 namespace LibrarySystem.API.Services;
 
 public interface IUserService
 {
-
+ public Task<User> CreateNewUserAsync(User userFromControllerClass);
 }
--- LibrarySystemAPI/04_DataAccess/UserDataAccess.cs
+++ LibrarySystemAPI/04_DataAccess/UserDataAccess.cs
@@ -5,5 +5,17 @@ namespace LibrarySystem.API.Data;
 
 public class UserDataAccess : IUserDataAccess
 {
+ private readonly LibrarySystemContext _context;
 
+  public UserDataAccess(LibrarySystemContext contextFromBuilder)
+    {
+        _context = contextFromBuilder;
+    }
+
+    public async Task<User?> CreateNewUserInDBAsync(User userFromServiceClass)
+    {
+        _context.Users.Add(userFromServiceClass); //_context establishing the connection iwth database , Insert into 
+        await _context.SaveChangesAsync();
+        return userFromServiceClass;
+    }
 }
