0ba2f4517bd1d00b7f3933285448b5988536509c
yennyFlores <yennyd.data@gmail.com>
2024-06-11 14:45:25 -0400

    changed for Mike renames

---
+++ LibrarySystemAPI/01_Models/UsernameUpdateDTO.cs
@@ -0,0 +1,7 @@
+namespace LibrarySystem.API.Models;
+
+public class UsernameUpdateDTO
+{
+    public string? oldUserName { get; set; } = string.Empty;
+    public string? newUserName { get; set;} = string.Empty;
+}
