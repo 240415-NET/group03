92c8eb79e0aeca5a374bfddb6b0ec220d478c215
Moreno <veda.moreno@yahoo.com>
2024-06-11 16:41:39 -0400

    Added ReadUser Changes

--- LibrarySystemAPI/04_DataAccess/DataAccessInterfaces/IUserDataAccess.cs
+++ LibrarySystemAPI/04_DataAccess/DataAccessInterfaces/IUserDataAccess.cs
@@ -7,4 +7,7 @@ public interface IUserDataAccess
   public Task<User?> CreateNewUserInDBAsync(User userFromServiceClass);
   public Task<string?> DeleteUserInDBAsync(string userFromServiceClass);
   public Task<string> UpdateUserInDBAsync(UsernameUpdateDTO usernamesToSwapFromUserService);
+  public Task<User?> GetUserFromDBByUsernameAsync(string usernameToFindFromUserService);
+  public Task<bool> DoesThisUserExistOnDBAsync(string usernameToFindFromUserService);
+
 }
