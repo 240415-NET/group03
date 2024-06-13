f0a0595b1bf4e5079ced08835c2daba22531ebc7
florespino <michael47265@yahoo.com>
2024-06-13 14:53:22 -0400

    added comments

--- LibrarySystemAPI/04_DataAccess/CheckoutDataAccess.cs
+++ LibrarySystemAPI/04_DataAccess/CheckoutDataAccess.cs
@@ -31,20 +31,22 @@ public class CheckoutDataAccess : ICheckoutDataAccess
 
     public async Task<List<Book>> booksAvailableForCheckoutAsync()
     {
-                //retrieve the list of checked out books
+                //retrieve the list of checked out books from the Checkouts table
                 List<Checkout> booksCheckedOut = await _checkoutContext.Checkouts.Where(u => u.status=="out")
                     .Include(u => u.checkoutBook)
                     .Include(u=> u.checkoutUser)
                     .ToListAsync();
                 //booksAvailable.ForEach(x=>Console.WriteLine($"{x.checkoutBook.barcode}-{x.status}"));
 
+                //retrieve the list of all books from the Books table
                 List<Book> booksAll = await _checkoutContext.Books.Select(x=>x).Where(x=>x.genre=="Fantasy").ToListAsync();
                 booksAll.ForEach(x=>Console.WriteLine($"{x.barcode}-{x.title}-{x.author}-{x.genre}"));
 
-                // List<Book> bF = booksAvailable.Select(x => x.checkoutBook.barcode)
-                //                 .Except(booksAvailable1.Select(x => x.barcode)).ToList();
+                //get the barcodes from CheckOut that we need to exclude
                 List<int> checkedOutBarCodes = booksCheckedOut.Select(d => d.checkoutBook.barcode).ToList();
                 //checkedOutBarCodes.ForEach(x=>Console.WriteLine(x));
+
+                //Exclude the barcodes found previously
                 List<Book> booksAvailable = booksAll.Where(x => !checkedOutBarCodes.Contains(x.barcode)).ToList();
                 //booksAvailable.ForEach(x=>Console.WriteLine($"{x.barcode}-{x.title}-{x.genre}"));
 
